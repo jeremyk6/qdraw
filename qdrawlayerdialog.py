5b795544a11a737608209a0045bb1b1837e9c60d
nobohan <juminet@gmail.com>
2019-02-12 16:24:59 +0100

    after qgis2to3 -w

--- qdrawlayerdialog.py
+++ qdrawlayerdialog.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt4.QtCore import QCoreApplication
-from PyQt4.QtGui import QDialog, QComboBox, QLineEdit
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtWidgets import QDialog, QComboBox, QLineEdit
 
 class QDrawLayerDialog(QDialog):
     def __init__(self, iface, gtype):
